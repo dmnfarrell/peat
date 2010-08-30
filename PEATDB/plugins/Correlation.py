5552db8c22e2a6142183cb3e583626c5077029b7
farrell.damien <farrell.damien@c6393abf-77d7-e6cc-b5c7-93784c25e86e>
2010-08-30 19:55:39 +0000

    license in plugins

--- PEATDB/plugins/Correlation.py
+++ PEATDB/plugins/Correlation.py
@@ -1,11 +1,31 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 #
-# This file is part Protein Engineering Analysis Tool (PEAT)
-# (C) Copyright Jens Erik Nielsen, University College Dublin 2003-
-# All rights reserved
+# Protein Engineering Analysis Tool DataBase (PEATDB)
+# Copyright (C) 2010 Damien Farrell & Jens Erik Nielsen
 #
-# Author: Damien Farrell, Feb 2010
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact information:
+# Email: Jens.Nielsen_at_gmail.com 
+# Normal mail:
+# Jens Nielsen
+# SBBS, Conway Institute
+# University College Dublin
+# Dublin 4, Ireland
+#
+
+"""Feb 2010"""
 
 try:
     from Plugins import Plugin
