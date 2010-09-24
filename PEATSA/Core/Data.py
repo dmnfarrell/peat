7c3787ad6b356b1b31514ac2311c8987d20f68ec
michael.ap.johnston <michael.ap.johnston@c6393abf-77d7-e6cc-b5c7-93784c25e86e>
2010-09-24 14:05:29 +0000

    Update for sequence offset

--- PEATSA/Core/Data.py
+++ PEATSA/Core/Data.py
@@ -1494,7 +1494,7 @@ def mutationSetFromSequencesAndStructure(initialSequence, targetSequence, struct
 		initialAA=initialSequence[count]
 		targetAA=targetSequence[count]
 		if initialAA != targetAA:
-			mutations.append([count, initialAA, targetAA])
+			mutations.append([count+1, initialAA, targetAA])
 	
 	#Align the initial sequence with each chain in the pdb
 	structure.Remove_All_NonAminoAcids()	
