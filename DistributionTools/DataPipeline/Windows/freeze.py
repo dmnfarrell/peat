90b182d61a8835a2d86f8d91fd862de168d4dc33
farrell.damien <farrell.damien@c6393abf-77d7-e6cc-b5c7-93784c25e86e>
2011-11-22 12:49:04 +0000

    added freeze script for pipeline app

---
+++ DistributionTools/DataPipeline/Windows/freeze.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+#bbfreeze setup file for DataPipeline distribution on Windows
+#Damien Farrell, #Nov 2011
+
+"""
+This script can be used to create a standalone executable for
+either windows or linux. It must be run on the target platform.
+You will need to install bbfreeze, see http://pypi.python.org/pypi/bbfreeze/
+"""
+
+from bbfreeze import Freezer
+import sys, os, shutil
+
+shutil.rmtree('datapipeline', ignore_errors=True)
+path=os.path.abspath('../../..')
+pipepath=os.path.abspath('../../../DataPipeline')
+peatpath=os.path.abspath('../../../PEATDB')
+version = '1.0'
+
+f = Freezer('datapipeline', excludes=('wx'))
+f.addScript(os.path.join(pipepath, "PipelineApp.py"))
+
+#these lines allow the plugins to work
+f.addModule('PEATDB.PEATApp')
+
+m=f.mf
+f()    # runs the freezing process
+
+'''post freeze'''
+#mpl data
+import matplotlib
+mpldir = matplotlib.get_data_path()
+datadir = 'datapipeline/mpl-data'
+shutil.copytree(mpldir, datadir)
+
+#add resource files
+resources = ['DataPipeline/app.ico',
+             'PEATDB/Ekin/models.dict']
+for r in resources:
+    shutil.copy(os.path.join(path, r), 'datapipeline')
+
+#make zip archive
+import zipfile
+f = zipfile.ZipFile("datapipeline-1.0.zip", "w")
+for dirpath, dirnames, filenames in os.walk('datapipeline'):
+    for fname in filenames:
+        fullname = os.path.join(dirpath, fname)        
+        f.write(fullname)
+f.close()
+
