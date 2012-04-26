8f3ea86a377d643c034648bd41f673867ece6f1e
farrell.damien <farrell.damien@c6393abf-77d7-e6cc-b5c7-93784c25e86e>
2012-04-26 17:57:54 +0000

    dist tools updates peatdb

--- DistributionTools/PEATDB/Linux/setup.py
+++ DistributionTools/PEATDB/Linux/setup.py
@@ -1,19 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
 import sys,os
 home=os.path.expanduser('~')
-pythonpath=os.path.join(home,'python')
 
-setup(name='PEATDB', version='2.0',
+setup(name='PEATDB', version='2.2',
 	description='Protein Engineering Analysis Tool',
 	author='Nielsen Group, UCD',
-	author_email='PEAT_support@ucd.ie',
+	author_email='farrell.damien[at]gmail.com',
 	url='http://enzyme.ucd.ie/PEAT',
-	license='For academic and non-profit use only. No redistribution, no alterations.',
-	packages=['PEATDB','PEATDB.DNAtool','PEATDB.Ekin',
-		  'Protool','pKaTool','DataPipeline'],
-	package_dir={'':pythonpath},
-	package_data={'DNAtool':['restriction_enzymes.DAT','DNAtool.ico'], 
-		      'PEAT_DB':['logo.gif','App.ico','data/AA_masses.txt'],
+	license='GPL v3',
+	packages=['PEATDB','PEATDB.DNAtool','PEATDB.Ekin','PEATSA.Core',
+		      'Protool','pKaTool'],
+	package_data={'PEATDB.DNAtool':['restriction_enzymes.DAT','DNAtool.ico'], 
+		      'PEATDB':['logo.gif','App.ico','data/AA_masses.txt'],
+              'PEATDB.Ekin':['models.dict'],
 		      'Protool':['AA.DAT','bbdep02.May.sortlib']},
-	requires=['Pmw','numpy'],
+    install_requires=['numpy>=1.1',
+                      'matplotlib>=0.98.5.2','ZODB3',
+                      'MySQL-python','relstorage>=1.4'],
+    entry_points = { 'gui_scripts': [
+                     'peatdb = PEATDB.PEATApp:main',
+                     'ekin= PEATDB.Ekin.Ekin_main:main'],}
 )
